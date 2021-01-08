6a69bc19f0cc1947b653838267054995ab4b7c5d
edselg <egarcia@progress.com>
2021-01-08 09:17:50 -0500

    Update

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -21,6 +21,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 
-IP_ADDRESS=`hostname -I | tr -d ' '`
-echo '{ "insecure-registries": [ "'${IP_ADDRESS}'" ] }' > /tmp/daemon.json
-sudo mv /tmp/daemon.json /etc/docker
+sudo cp daemon.json /etc/docker
