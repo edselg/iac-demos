76c054df7c19696e81b470b3569922e664dd9dfd
edselg <egarcia@progress.com>
2021-01-08 08:25:03 -0500

    Update

--- create_k3d_cluster.sh
+++ create_k3d_cluster.sh
@@ -3,5 +3,4 @@ k3d cluster create demo \
     --port 5050:80@loadbalancer \
     --port 8080:8080@loadbalancer \
     --port 9090:9090@loadbalancer \
-    --port "30000-32767:30000-32767@server[0]" \
-    --volume "/install/registries.yaml:/etc/rancher/k3s/registries.yaml"
+    --volume "/home/progress/iac-demos/registries.yaml:/etc/rancher/k3s/registries.yaml"
